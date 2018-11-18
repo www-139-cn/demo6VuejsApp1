28b43b5d462b6d0b109306666615dd1dac8f7d9e
112906505 <112906505@qq.com>
2018-11-18 22:59:26 +0800

    添加项目文件。

---
+++ VuejsApp1/src/components/Home.vue
@@ -0,0 +1,22 @@
+﻿
+<template>
+    <!--meta charset="utf-8"-->
+    <div class="home">
+        <h1>{{ msg }}</h1>
+        欢迎光临山哥的网址, built with <a href="https://vuejs.org" target="_blank">Vue.js</a>.
+        {{ msg1 }}
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Home',
+        props: {
+            msg: String
+        }
+    };
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
